a593bb73dd386fb44fffde5d3dbd8d6846ffd2ab
zhy <3234079912@qq.com>
2026-07-07 19:03:24 +0800

    feat: add entity, mapper, service (User, Role, Doctor)

---
+++ src/main/java/com/smarteldercare/modules/system/entity/Role.java
@@ -0,0 +1,63 @@
+package com.smarteldercare.modules.system.entity;
+
+import java.time.LocalDateTime;
+
+public class Role {
+    private Long id;
+    private String roleName;
+    private String roleCode;
+    private String description;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+}
