0977ae1e6e571f259061412d112da5f76002230f
chaste <15663773235@163.com>
2026-07-08 15:32:03 +0800

    fix: add MapperScan annotation

--- src/main/java/com/smarteldercare/SmartElderCareApplication.java
+++ src/main/java/com/smarteldercare/SmartElderCareApplication.java
@@ -2,8 +2,10 @@ package com.smarteldercare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.smarteldercare.modules.**.mapper")
 public class SmartElderCareApplication {
 
     public static void main(String[] args) {
