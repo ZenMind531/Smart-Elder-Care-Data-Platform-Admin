21ae5030d28bb7468ece7b25c075eb406fbd3ebc
zhy <3234079912@qq.com>
2026-07-10 14:04:45 +0800

    feat: add doctor/nurse registration with role-based fields

--- src/main/java/com/smarteldercare/modules/system/dto/RegisterRequest.java
+++ src/main/java/com/smarteldercare/modules/system/dto/RegisterRequest.java
@@ -3,12 +3,50 @@ package com.smarteldercare.modules.system.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
+
+
 @Getter
 public class RegisterRequest {
     private String username;
     private String password;
     private String realName;
     private String phoneNumber;
+    private Long RoleId;
 
+    private String gender;//性别
+
+    private String department;//科室
+    private String title;//职称
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setRoleId(Long roleId) {
+        RoleId = roleId;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
